8429069f95e24154c7f95d95f7d3da4e1095d7ef
Nicolas Hermet <n.hermet@gmail.com>
2019-01-14 15:22:26 +0100

    Fix a style guide on automatique comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,8 +29,8 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
     this.appareils = this.appareilService.appareils;
   }
 
