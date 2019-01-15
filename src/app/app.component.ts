0e42cd3fba54dc5c88d78e9b113d4edb0d0d8b60
Nicolas Hermet <n.hermet@gmail.com>
2019-01-15 16:43:34 +0100

    Migrate appareil views from app.component to appareil-view.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppareilsService } from './services/appareils.service';
 
 @Component({
@@ -6,44 +6,4 @@ import { AppareilsService } from './services/appareils.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  isAuth = false;
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-
-  appareils: any[];
-
-
-  constructor(private appareilService: AppareilsService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-      );
-  }
-
-  ngOnInit(): void {
-    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    // Add 'implements OnInit' to the class.
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-    if (confirm('Il va faire tout noir ?')) {
-      this.appareilService.switchOffAll();
-    } else {
-      return null;
-    }
-  }
-
-}
+export class AppComponent { }
--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppareilsService } from '../services/appareils.service';
 
 @Component({
   selector: 'app-appareil-view',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appareil-view.component.scss']
 })
 export class AppareilViewComponent implements OnInit {
+  isAuth = false;
+  appareils: any[];
 
-  constructor() { }
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
 
-  ngOnInit() {
+  constructor(private appareilService: AppareilsService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+      );
+  }
+
+  ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+
+  onEteindre() {
+    if (confirm('Il va faire tout noir ?')) {
+      this.appareilService.switchOffAll();
+    } else {
+      return null;
+    }
   }
 
 }
