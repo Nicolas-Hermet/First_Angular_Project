358db8c1c09081b002d495f2b37a0c2bc755815c
Nicolas Hermet <n.hermet@gmail.com>
2019-01-22 10:56:07 +0100

    Add route and import ReactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonPremierComponentComponent } from './mon-premier-component/mon-premier-component.component';
 import { AppareilComponent } from './appareil/appareil.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppareilsService } from './services/appareils.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
@@ -16,12 +16,14 @@ import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { AuthGuardService } from './services/auth-guard.service';
 import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
 import { UserService } from './services/user.service';
+import { UserlistComponent } from './userlist/userlist.component';
 
 const appRoutes: Routes = [
   {path: 'appareils', canActivate: [AuthGuardService], component: AppareilViewComponent},
   {path: 'auth', component: AuthComponent},
   {path: 'appareils/:id', canActivate: [AuthGuardService], component: SingleAppareilComponent},
-  { path: 'edit', canActivate: [AuthGuardService], component: EditAppareilComponent },
+  {path: 'edit', canActivate: [AuthGuardService], component: EditAppareilComponent },
+  {path: 'users', component: UserlistComponent},
   {path: '',  canActivate: [AuthGuardService], component: AppareilViewComponent},
   {path: 'not-found', component: FourOhFourComponent},
   {path: '**', redirectTo: 'not-found'}
@@ -36,11 +38,13 @@ const appRoutes: Routes = [
     AppareilViewComponent,
     SingleAppareilComponent,
     FourOhFourComponent,
-    EditAppareilComponent
+    EditAppareilComponent,
+    UserlistComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes)
   ],
